f0aa8df88b6d9cb9739a24deb64bcd45ba3a508f
tesh254 <ewachira254@gmail.com>
2019-10-31 04:32:39 +0300

    - render a marker based on a users location

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
+import MarkerIcon from "../static/imgs/marker.png";
 
 class Map extends React.Component {
   // Default state when a user vists a site
@@ -7,27 +8,35 @@ class Map extends React.Component {
     viewport: {
       width: "100vw",
       height: "100vh",
-      zoom: 16,
+      zoom: 9,
       latitude: 42.430472,
       longitude: -123.334102
-    }
+    },
+    userLocation: {
+        lat: 42.430472,
+        long: -123.334102}
   };
 
   setUserLocation = () => {
     //   Allows us to get the current user's location
     navigator.geolocation.getCurrentPosition(position => {
-        console.log(position.coords, '>>>>>>>>>>>>>>>>')
+      // Define user location to overide current app state
+      let newUserLocation = {
+        lat: position.coords.latitude,
+        long: position.coords.longitude
+      };
       //   Define the current location of user
       let newViewPort = {
         ...this.state,
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
-        zoom: 12
+        zoom: 10
       };
 
       //   Update our component state
       this.setState({
-        viewport: newViewPort
+        viewport: newViewPort,
+        userLocation: newUserLocation
       });
     });
   };
@@ -36,6 +45,9 @@ class Map extends React.Component {
   // mapboxApiAccessToken grants us access to interact with mapbox maps
 
   render() {
+    const {
+      userLocation: { long, lat }
+    } = this.state;
     return (
       <div className="map-holder">
         <button onClick={this.setUserLocation}>find me</button>
@@ -49,7 +61,16 @@ class Map extends React.Component {
             {...this.state.viewport}
             mapStyle="mapbox://styles/mapbox/outdoors-v11"
             mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-          ></ReactMapGL>
+          >
+            {/* Couple of error handling such that if we lack the data rather than a red screen present a readable error */}
+            {Object.keys(this.state.userLocation).length !== 0 ? (
+              <Marker longitude={long} latitude={lat}>
+                <img src={MarkerIcon} alt="marker"/>
+              </Marker>
+            ) : (
+              <div>Empty</div>
+            )}
+          </ReactMapGL>
         </section>
       </div>
     );
