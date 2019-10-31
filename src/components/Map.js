a06f7cca5a6006257f6ae8984990cbd6dfd5ada8
tesh254 <ewachira254@gmail.com>
2019-10-31 04:10:11 +0300

    - able to fetch current user location using navigator object

--- src/components/Map.js
+++ src/components/Map.js
@@ -12,23 +12,46 @@ class Map extends React.Component {
       longitude: -123.334102
     }
   };
+
+  setUserLocation = () => {
+    //   Allows us to get the current user's location
+    navigator.geolocation.getCurrentPosition(position => {
+        console.log(position.coords, '>>>>>>>>>>>>>>>>')
+      //   Define the current location of user
+      let newViewPort = {
+        ...this.state,
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        zoom: 12
+      };
+
+      //   Update our component state
+      this.setState({
+        viewport: newViewPort
+      });
+    });
+  };
+
   // onViewPortChange gives the user the ability to interact with the map
   // mapboxApiAccessToken grants us access to interact with mapbox maps
 
   render() {
     return (
-      <ReactMapGL
-        onViewportChange={viewport =>
-          this.setState({
-            viewport
-          })
-        }
-        {...this.state.viewport}
-        mapStyle="mapbox://styles/mapbox/outdoors-v11"
-        mapboxApiAccessToken={
-          process.env.REACT_APP_MAPBOX_TOKEN
-        }
-      ></ReactMapGL>
+      <div className="map-holder">
+        <button onClick={this.setUserLocation}>find me</button>
+        <section className="map">
+          <ReactMapGL
+            onViewportChange={viewport =>
+              this.setState({
+                viewport
+              })
+            }
+            {...this.state.viewport}
+            mapStyle="mapbox://styles/mapbox/outdoors-v11"
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          ></ReactMapGL>
+        </section>
+      </div>
     );
   }
 }
